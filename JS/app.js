a80d91cafce84f80c1eebc7ee4d9ae11f56e38d7
Turns <fix.proout@gmail.com>
2024-04-01 15:40:53 +1000

    Added datafilter

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,31 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-
-
-let submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', function () {
-
-    var filter = "";
-    
-    var category = document.getElementById('categorySelector');
-    var lineopen = document.getElementById('lineopencheckbox');
-    var coef = document.getElementById('coefcheckbox')
-
-    tg.sendData(filter);
-});
-
 var backgrounds = [
   "background: -webkit-linear-gradient(310deg, #4a19dd,#dcf8f0); background: linear-gradient(310deg, #4a19dd,#dcf8f0);",
 ];
 
-// Выбор случайного значения из массива backgrounds
+document.addEventListener('DOMContentLoaded', function() {
+  tg.MainButton.show();
+});
 
-//var randomIndex = Math.floor(Math.random() * (backgrounds.length + 1));
-//var randomBackground = backgrounds[randomIndex];
-//document.body.style = randomBackground;
 
-//$(document).ready(function() {
-//    $('.js-example-basic-single').select2();
-//});
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  try {
+    var filter = "";
+    
+    var category = document.getElementById('categorySelector');
+    var lineopen = document.getElementById('lineopencheckbox');
+    var coef = document.getElementById('coefcheckbox');
+    
+    filter = `${category.value}[SEPARATOR]${lineopen.checked}[SEPARATOR]${coef.checked}`
+
+    tg.sendData(filter);
+    
+  }
+  catch (e) {
+    // инструкции для обработки ошибок
+    alert(e); // передать объект исключения обработчику ошибок
+  }
+});
 
